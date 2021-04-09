e6cbc7bf0aa6e79d27571448414862018836d09e
hans0 <kiessler.hans@gmail.com>
2021-04-09 13:34:40 -0400

    Initial commit, added to plant array, set up custom hooks

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,23 @@
+import useLocalStorage from './useLocalStorage';
+
 // write your custom hook here to control your checkout form
+const useForm = (key, initialValue) => {
+  const [values, setValue] = useLocalStorage("form", initialValues);
+    
+    const handleChanges = e => {
+      setValue({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    const clearForm = e => {
+      e.preventDefault();
+      setValue(initialValues);
+    };
+  
+    return([ values, handleChanges, clearForm ]);
+}
+}
+
+export default useForm;
